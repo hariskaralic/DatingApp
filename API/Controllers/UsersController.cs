d68568cd4dfd8235a40eb67165e301ab4778d743
Haris Karalic <harisk@comtrade.com>
2022-08-22 14:55:48 +0200

    First commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        
+        {
+            return  await _context.Users.ToListAsync();
+        }
+
+
+        // GET api/<UsersController>/5
+        [HttpGet("{Id}")]
+        public async  Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return  await _context.Users.FindAsync(id);
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
